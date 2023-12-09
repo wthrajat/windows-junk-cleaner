bf1cb76268c008b1fcaf4d378291ba88f75c6a4a
Rajat Khanduri <rajatkhanduri290102@gmail.com>
2023-12-09 23:01:51 +0530

    improvements
    Signed-off-by: Rajat Khanduri <rajatkhanduri290102@gmail.com>

--- junk.py
+++ junk.py
@@ -1,114 +1,94 @@
-import tkinter
-import tkinter.messagebox
+import tkinter as tk
+from tkinter import ttk, messagebox
 import os
 import os.path
 import threading
+from ttkthemes import ThemedStyle
 
-rubbishExt = ['.tmp', '.bak', '.old', '.wbk', '.xlk', '_mp', '.gid',
-              '.chk', '.syd', '.$$$', '.@@@', ".~*"]
-
+rubbishExt = ['.tmp',
+                '.bak',
+                '.old',
+                '.wbk',
+                '.xlk',
+                '_mp',
+                '.gid',
+                '.chk',
+                '.syd',
+                '.$$$',
+                '.@@@',
+                ".~*"]
 
 def GetDrives():
-    drives = []
-    for i in range(65, 91):
-        vol = chr(i) + ":/"
-        if os.path.isdir(vol):
-            drives.append(vol)
+    drives = [f"{chr(i)}:/" for i in range(65, 91) if os.path.isdir(f"{chr(i)}:/")]
     return tuple(drives)
-
 
 class Window:
     def __init__(self):
-        self.root = tkinter.Tk()
+        self.root = tk.Tk()
+        self.root.title("Junk Cleaner")
+        self.root.geometry("720x512")
+
+        # Apply a themed style
+        self.style = ThemedStyle(self.root)
+        self.style.set_theme("adapta")  # You can try different themes
 
         # Create menu.
-        menu = tkinter.Menu(self.root)
+        menu_bar = tk.Menu(self.root, font=('Arial', 13))
 
-        # Create 'System' submenu
-        submenu = tkinter.Menu(menu, tearoff=0)
-        submenu.add_command(label="About", command=self.MenuAbout)
-        submenu.add_separator()
-        submenu.add_command(label="Exit", command=self.MenuExit)
-        menu.add_cascade(label="Info", menu=submenu)
+        system_menu = tk.Menu(menu_bar, tearoff=0, font=('Arial', 13))
+        clean_menu = tk.Menu(menu_bar, tearoff=0, font=('Arial', 13))
 
-        # Create 'Clean' submenu
-        submenu = tkinter.Menu(menu, tearoff=0)
-        submenu.add_command(label="Scan", command=self.MenuScanRubbish)
-        submenu.add_command(label="Delete", command=self.MenuDelRubbish)
-        menu.add_cascade(label="Clean", menu=submenu)
-
-        # Create 'Search' submenu
-        submenu = tkinter.Menu(menu, tearoff=0)
-        submenu.add_command(label="Search Big Files",
-                            command=self.MenuScanBigFile)
-        submenu.add_separator()
-        submenu.add_command(label="Search Big Files By Name",
-                            command=self.MenuSearchFile)
-        menu.add_cascade(label="Search", menu=submenu)
-
-        self.root.config(menu=menu)
+        clean_menu.add_command(label="Scan…", command=self.MenuScanRubbish)
+        """system_menu.add_separator()"""
+        clean_menu.add_command(label="Clean…", command=self.MenuDelRubbish)
+        menu_bar.add_cascade(label="Junk", menu=clean_menu)
+        
+        system_menu.add_command(label="About", command=self.MenuAbout)
+    
+        system_menu.add_command(label="Exit", command=self.MenuExit)
+        menu_bar.add_cascade(label="Info", menu=system_menu)
+        
+        self.root.config(menu=menu_bar)
 
         # Create labels to show info
-        self.progress = tkinter.Label(self.root, anchor=tkinter.W,
-                                      text='Status', bitmap='hourglass',
-                                      compound='left')
-        self.progress.place(x=10, y=370, width=480, height=15)
+        self.progress_var = tk.StringVar()
+        self.progress_var.set(' Status…')
+        self.progress = ttk.Label(self.root, textvariable=self.progress_var, anchor=tk.W)
+        self.progress.place(x=10, y=370, width=720, height=25)
 
         # Create text box to show file list
-        self.flist = tkinter.Text(self.root)
-        self.flist.place(x=10, y=10, width=480, height=350)
+        self.flist = tk.Text(self.root, wrap="none", font=('Arial', 15))
+        self.flist.place(x=10, y=10, width=720, height=350)
 
         # Create scroll bar for text box
-        self.vscroll = tkinter.Scrollbar(self.flist)
-        self.vscroll.pack(side='right', fill='y')
+        self.vscroll = ttk.Scrollbar(self.root, command=self.flist.yview)
+        self.vscroll.place(x=700, y=10, height=350)
         self.flist['yscrollcommand'] = self.vscroll.set
-        self.vscroll['command'] = self.flist.yview
 
     def MainLoop(self):
-        self.root.title("GGD Cleaner")
-        self.root.minsize(500, 400)
-        self.root.maxsize(500, 400)
         self.root.mainloop()
 
     def MenuAbout(self):
-        tkinter.messagebox.showinfo("GGD Cleaner",
-                                    "This simple tool cleans junk aka GGD from your PC :)")
+        messagebox.showinfo("Junk Cleaner", "Scan and delete trash from your PC")
 
     def MenuExit(self):
         self.root.quit()
 
     def MenuScanRubbish(self):
-        result = tkinter.messagebox.askquestion("GGD Cleaner", "Scan now?")
+        result = messagebox.askquestion("Junk Cleaner", "Scan now?")
         if result == 'no':
             return
-
-        # self.ScanRubbish()
         self.drives = GetDrives()
         t = threading.Thread(target=self.ScanRubbish, args=(self.drives,))
         t.start()
 
     def MenuDelRubbish(self):
-        result = tkinter.messagebox.askquestion("GGD Cleaner",
-                                                "Delete GGD Junk?")
+        result = messagebox.askquestion("Junk Cleaner", "Delete all junk?")
         if result == 'no':
             return
-        #tkinter.messagebox.showinfo("GGD Cleaner", "Cleaning GGD...")
         self.drives = GetDrives()
         t = threading.Thread(target=self.DeleteRubbish, args=(self.drives,))
         t.start()
-
-    def MenuScanBigFile(self):
-        result = tkinter.messagebox.askquestion("GGD Cleaner",
-                                                "Scan now?")
-        if result == 'no':
-            return
-        tkinter.messagebox.showinfo("GGD Cleaner", "Scanning...")
-
-    def MenuSearchFile(self):
-        result = tkinter.messagebox.askquestion("GGD Cleaner",
-                                                "Searching big files?")
-        if result == 'no':
-            return
 
     def ScanRubbish(self, scanpath):
         global rubbishExt
@@ -119,30 +99,22 @@ class Window:
                 try:
                     for fil in files:
                         filesplit = os.path.splitext(fil)
-                        if filesplit[1] == '':
-                            continue
-                        try:
-                            if rubbishExt.index(filesplit[1]) >= 0:
-                                fname = os.path.join(
-                                    os.path.abspath(root), fil)
-                                filesize += os.path.getsize(fname)
-                                if total % 20 == 0:
-                                    self.flist.delete(0.0, tkinter.END)
-                                self.flist.insert(tkinter.END, fname+'\n')
-                                l = len(fname)
-                                if l > 60:
-                                    self.progress['text'] = fname[:30] + '...' + \
-                                        fname[l-30:l]
-                                else:
-                                    self.progress['text'] = fname
-                                total += 1
-                        except ValueError:
-                            pass
+                        if filesplit[1] in rubbishExt:
+                            fname = os.path.join(os.path.abspath(root), fil)
+                            filesize += os.path.getsize(fname)
+                            if total % 20 == 0:
+                                self.flist.delete(1.0, tk.END)
+                            self.flist.insert(tk.END, fname + '\n')
+                            l = len(fname)
+                            if l > 60:
+                                self.progress_var.set(fname[:30] + '...' + fname[l-30:l])
+                            else:
+                                self.progress_var.set(fname)
+                            total += 1
                 except Exception as e:
                     print(e)
                     pass
-        self.progress['text'] = "Found %s junk files, occupying %.2f MB Disk Space!" \
-            % (total, filesize/1024/1024)
+        self.progress_var.set(f"Found {total} junk files, occupying {filesize/1024/1024:.2f} MB disk space!")
 
     def DeleteRubbish(self, scanpath):
         global rubbishExt
@@ -153,34 +125,25 @@ class Window:
                 try:
                     for fil in files:
                         filesplit = os.path.splitext(fil)
-                        if filesplit[1] == '':
-                            continue
-                        try:
-                            if rubbishExt.index(filesplit[1]) >= 0:
-                                fname = os.path.join(
-                                    os.path.abspath(root), fil)
-                                filesize += os.path.getsize(fname)
-                                try:
-                                    os.remove(fname)
-                                    l = len(fname)
-                                    if l > 50:
-                                        fname = fname[:25] + "..." + \
-                                            fname[l-25:l]
-                                    if total % 15 == 0:
-                                        self.flist.delete(0.0, tkinter.END)
-                                    self.flist.insert(tkinter.END, 'Deleted '
-                                                      + fname + '\n')
-                                    self.progress['text'] = fname
-                                    total += 1
-                                except:
-                                    pass
-                        except ValueError:
-                            pass
+                        if filesplit[1] in rubbishExt:
+                            fname = os.path.join(os.path.abspath(root), fil)
+                            filesize += os.path.getsize(fname)
+                            try:
+                                os.remove(fname)
+                                l = len(fname)
+                                if l > 50:
+                                    fname = fname[:25] + "..." + fname[l-25:l]
+                                if total % 15 == 0:
+                                    self.flist.delete(1.0, tk.END)
+                                self.flist.insert(tk.END, 'Deleted ' + fname + '\n')
+                                self.progress_var.set(fname)
+                                total += 1
+                            except:
+                                pass
                 except Exception as e:
                     print(e)
                     pass
-        self.progress['text'] = f"Deleted {filesize/1024/1024} MB junk files = Recovered {filesize/1024/1024} MB Disk Space :P"
-
+        self.progress_var.set(f"Deleted {filesize/1024/1024:.2f} MB junk files = Recovered {filesize/1024/1024:.2f} MB disk space.")
 
 if __name__ == "__main__":
     window = Window()
